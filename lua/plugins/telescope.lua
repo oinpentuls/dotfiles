e18a8ee802a685ae324d48fc4e2100b2cd6ca8ab
Ahmad Tohir <achmad.tohier@gmail.com>
2022-04-07 11:14:17 +0700

    add live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 local map = vim.api.nvim_set_keymap
 
 map('n', '<C-p>', [[:Telescope find_files<CR>]], {noremap=true})
-map('n', ';r', [[:Telescope find_files<CR>]], {noremap=true})
+map('n', ';r', [[:Telescope live_grep<CR>]], {noremap=true})
 
